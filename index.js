a297bf726c8076594dd31ea89cece3283acf57e2
Gil Gonçalves <lursty@gmail.com>
2014-06-28 11:40:07 +0100

    Now sending data to pubnub

--- index.js
+++ index.js
@@ -2,18 +2,32 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var pubnub = require("pubnub").init({
+    publish_key   : "demo",
+    subscribe_key : "demo"
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 app.use(express.static(__dirname + '/public'));
 
-
 app.post('/', function(req,res){
-
-	res.send("ok! - " + req.param('id'));
+    var message = {'did' : req.param('id')};
+    pubnub.publish({
+        channel: 'do_channel',
+        message: message,
+        callback: function(e) {
+            res.send("OK! Tracking: " + req.param('id'));
+            console.log( "SUCCESS!", e );
+        },
+        error: function(e) {
+            res.send("FAIL! Tracking: " + req.param('id'));
+            console.log( "FAILED! RETRY PUBLISH!", e );
+        }
+    });
 
 })
-
 
 var PORT = process.env.PORT || 3000;
 
