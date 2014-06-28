80a515ee7c75a1cbe9ae4c711bbc56907c7d85a1
Gil Gonçalves <lursty@gmail.com>
2014-06-28 13:09:19 +0100

    Added mongoose and persistance storage of habits done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+
 var CHANNEL_NAME = 'do_channel';
 
 var PUBNUB = require("pubnub").init({
@@ -8,23 +11,51 @@ var PUBNUB = require("pubnub").init({
     subscribe_key : "demo"
 });
 
+mongo_db_table = process.env.MONGOHQ_URL || 'mongodb://localhost:27017/test';
+mongoose.connect(mongo_db_table);
+
+// Models
+var Habit = mongoose.model(
+    'Habit', {
+        name: String,
+    }
+);
+
+var HabitDone = mongoose.model(
+    'HabitDone', {
+        habit: String,
+        when: Date,
+    }
+);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 app.use(express.static(__dirname + '/public'));
 
 app.post('/', function(req, res){
-    var message = {'did' : req.param('id')};
+    var habit_id = req.param('id');
+    var message = {'did' : habit_id};
     PUBNUB.publish({
         channel: CHANNEL_NAME,
         message: message,
         callback: function(e) {
-            res.send("OK! Tracking: " + req.param('id'));
-            console.log( "SUCCESS!", e );
+
+            // Save habit
+            var habit_done = new HabitDone({ habit: habit_id });
+            habit_done.save(function (err) {
+                if (err) {
+                    console.log('Error storing: ' + habit_done);
+                }
+            });
+
+            // Show counter
+            HabitDone.count({'habit': habit_id}, function(err, count) {
+                res.send(count);
+            });
         },
         error: function(e) {
             res.send("FAIL! Tracking: " + req.param('id'));
-            console.log( "FAILED! RETRY PUBLISH!", e );
         }
     });
 });
