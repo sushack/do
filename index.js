fbf732f5e7bc86db3673cc583c34d3410fe6b77c
Ben Foxall <benfoxall@gmail.com>
2014-06-28 11:13:52 +0100

    support post requests

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use(express.static(__dirname + '/public'));
+
+
+app.post('/', function(req,res){
+
+	res.send("ok! - " + req.param('id'));
+
+})
+
 
 var PORT = process.env.PORT || 3000;
 
