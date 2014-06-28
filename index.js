dd379e0cbdfbe3779b1ff3d629fcca476c4f78e7
Ben Foxall <benfoxall@gmail.com>
2014-06-28 11:04:14 +0100

    move to express

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(process.env.PORT || 3000);
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
